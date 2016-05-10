a58504aeefd92e5c097daac2a28d02c3d9e8869a
lantie <geekmade@qq.com>
2016-05-11 06:25:58 +0800

    first
    第一次上传

---
+++ src/My15pbPack/My15pbPackDlg.h
@@ -0,0 +1,38 @@
+
+// My15pbPackDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMy15pbPackDlg 对话框
+class CMy15pbPackDlg : public CDialogEx
+{
+// 构造
+public:
+	CMy15pbPackDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MY15PBPACK_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strPath;
+	afx_msg void OnBnClickedOpenfile();
+	afx_msg void OnBnClickedPack();
+	CString m_strKey;
+	afx_msg void OnDropFiles(HDROP hDropInfo);
+};
